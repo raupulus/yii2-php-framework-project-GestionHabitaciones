74c1af9b62c1d6774636bd07c2be2f942f86b668
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-30 19:25:28 +0100

    Establece datos de conexión para desarrollo

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=gestionarhabitaciones',
+    'username' => 'gestionarhabitaciones',
+    'password' => 'gestionarhabitaciones',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
