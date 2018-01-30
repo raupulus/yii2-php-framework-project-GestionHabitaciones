a836390ca490d6437e98918a404b5ce3fef3f52e
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-30 19:29:21 +0100

    Configura base de datos de pruebas

--- config/test_db.php
+++ config/test_db.php
@@ -1,6 +1,6 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=yii2_basic_tests';
+$db['dsn'] = 'psql:host=localhost;dbname=gestionarhabitaciones_test';
 
 return $db;
