287ef6010920c9b506e4a485abe30f3c734b2518
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-14 14:48:25 +0100

    Configuración de Base de Datos

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=gestionarhabitaciones',
-    'username' => 'gestionarhabitaciones',
-    'password' => 'gestionarhabitaciones',
+    'dsn' => 'pgsql:host=localhost;dbname=gestionhabitaciones',
+    'username' => 'gestionhabitaciones',
+    'password' => 'gestionhabitaciones',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
