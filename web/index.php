17c4a66a789096abf6f8f4c1544209563eb44445
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-28 13:51:46 +0100

    Asigno desarrollo para depurar

--- web/index.php
+++ web/index.php
@@ -1,12 +1,12 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-
+/*
 if (($getenv = getenv('YII_ENV')) !== 'prod'
     && file_exists($file = __DIR__ . '/../.env')) {
     (new \Symfony\Component\Dotenv\Dotenv())->load($file);
 }
-
+*/
 define('YII_ENV', $getenv ?: 'dev');
 define('YII_DEBUG', getenv('YII_DEBUG') ?: YII_ENV == 'dev');
 
